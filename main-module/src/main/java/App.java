30b73ebfb1c46197f4c7e7d01eeaa48d30f33f3d
aidev <aidev@192.168.29.157>
2026-08-18 23:27:18 +0530

    Initial code structure commit.

---
+++ main-module/src/main/java/App.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * Project execution will start using this class.
+ */
+public final class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Welcome to first project :-)");
+
+	}
+
+}
